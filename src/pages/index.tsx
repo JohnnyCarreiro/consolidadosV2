bac54345af774d3b20e6a2382869eafd6e8bfa93
Johnny <jcarreiropublicidade@gmail.com>
2021-12-10 00:15:07 -0300

    feat: Create state and toggle function to handle menu open and close state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react'
 import Navbar from '@/components/Navbar'
 import { Sidebar } from '@/components/Sidebar'
 import Head from 'next/head'
 import { Container } from '../styles/Home'
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <Container>
       <Head>
         <title>Template Next Tipescript</title>
       </Head>
-      <Sidebar isOpen={false} />
-      <Navbar/>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
       <section>
         <h1>Template using typescript and Sass</h1>
       </section>
