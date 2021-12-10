f25704bd9695993a301b305401c9349820e9d67c
Johnny <jcarreiropublicidade@gmail.com>
2021-12-10 10:46:40 -0300

    feat: Create and style content section

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
+import Link from 'next/link'
 
-import { HeroBg, HeroContainer, VideoBg } from './styles'
+import { ArrowForward, ArrowRight, HeroBg, HeroBtn, HeroBtnWrapper, HeroContainer, HeroContent, HeroH1, HeroP, VideoBg } from './styles'
 import Video from '@/assets/hero_video.mp4'
 
 interface HeroSectionProps {
@@ -19,6 +20,19 @@ function HeroSection({ children }: HeroSectionProps) {
           // type="video/mp4"
         />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Sua empresa faz negócios on-line?</HeroH1>
+        <HeroP>
+          Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Link prefetch href='/signup' passHref >
+            <HeroBtn>
+              De volta ao negócios {hover ? <ArrowForward/> : <ArrowRight/>}
+            </HeroBtn>
+          </Link>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
