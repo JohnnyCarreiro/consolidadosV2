e730c481c6292761d40144d658691534244b96c3
Johnny <jcarreiropublicidade@gmail.com>
2021-12-12 20:55:46 -0300

    feat: Prepare for use next image pluging to handle with imports

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 const withVideos = require('next-videos')
+const withImages = require('next-images')
+const path = require('path');
 
-module.exports = withVideos()
+module.exports = withVideos(), withImages({
+  esModule: true,
+  inlineImageLimit: false,
+  webpack(config, options) {
+    return config
+  }
+})
