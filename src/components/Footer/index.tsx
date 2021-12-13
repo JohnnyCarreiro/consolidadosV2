b243159ae20b7608fa0d660eb1b0d2f0b111d94c
Johnny <jcarreiropublicidade@gmail.com>
2021-12-13 13:55:46 -0300

    feat: Add is path to perform scroll

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -22,40 +22,200 @@ function Footer({ children }: FooterProps) {
           <FooterLinksWrapper>
             <FooterLinksItems>
               <FooterLinksTitle>About Us</FooterLinksTitle>
-              <FooterLink>How it works</FooterLink>
-              <FooterLink>Testimonial</FooterLink>
-              <FooterLink>Careers</FooterLink>
-              <FooterLink>Investors</FooterLink>
-              <FooterLink>Terms of service</FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                How it works
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Testimonial
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Careers
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Investors
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Terms of service
+              </FooterLink>
             </FooterLinksItems>
 
             <FooterLinksItems>
-              <FooterLinksTitle>Services</FooterLinksTitle>
-              <FooterLink>How it works</FooterLink>
-              <FooterLink>Testimonial</FooterLink>
-              <FooterLink>Careers</FooterLink>
-              <FooterLink>Investors</FooterLink>
-              <FooterLink>Terms of service</FooterLink>
+              <FooterLinksTitle>Contact Us</FooterLinksTitle>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Contact
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Address
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Support
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Destination
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Sporsorships
+              </FooterLink>
             </FooterLinksItems>
           </FooterLinksWrapper>
           {/* Second Column */}
           <FooterLinksWrapper>
-            <FooterLinksItems>
-              <FooterLinksTitle>About Us</FooterLinksTitle>
-              <FooterLink>How it works</FooterLink>
-              <FooterLink>Testimonial</FooterLink>
-              <FooterLink>Careers</FooterLink>
-              <FooterLink>Investors</FooterLink>
-              <FooterLink>Terms of service</FooterLink>
+          <FooterLinksItems>
+              <FooterLinksTitle>Social</FooterLinksTitle>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Youtube
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Facebook
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Instagram
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Linked In
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Twitter
+              </FooterLink>
             </FooterLinksItems>
 
             <FooterLinksItems>
               <FooterLinksTitle>Services</FooterLinksTitle>
-              <FooterLink>How it works</FooterLink>
-              <FooterLink>Testimonial</FooterLink>
-              <FooterLink>Careers</FooterLink>
-              <FooterLink>Investors</FooterLink>
-              <FooterLink>Terms of service</FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Service 1
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Service 2
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Service 3
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Service 4
+              </FooterLink>
+              <FooterLink
+                to={'home'}
+                smooth={true}
+                duration={500}
+                spy={true}
+                offset={-80}
+              >
+                Terms of service with some long text, but not long enaought;
+              </FooterLink>
             </FooterLinksItems>
           </FooterLinksWrapper>
 
