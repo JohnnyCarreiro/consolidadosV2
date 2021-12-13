5f100b9572eedc737d1273a8ac9a9c08ff510edc
Johnny <jcarreiropublicidade@gmail.com>
2021-12-12 21:32:28 -0300

    feat: Fecthing data from data objects

--- src/components/InfoSection/index.tsx
+++ src/components/InfoSection/index.tsx
@@ -1,31 +1,54 @@
 import { ReactNode } from 'react'
 import { Button } from '../Button/ButtonElements'
+import { InfoSectionProps } from './entity'
 
 import { BtnWrapper, Column1, Column2, Heading, Img, ImgWrapper, InfoContainer, InfoRow, InfoWrapper, Subtitle, TextWrapper, TopLine } from './styles'
 
-interface InfoSectionProps {
-  children?: ReactNode
-  id: string
-}
-
-export const InfoSection:React.FC<InfoSectionProps> = ({ children, id }) => {
+export const InfoSection:React.FC<InfoSectionProps> = (infoSectionProps) => {
+  const {
+    id,
+    lightBg,
+    lightText,
+    lightTextDesc,
+    topLine,
+    headLine,
+    description,
+    buttonLabel,
+    imgStart,
+    img,
+    alt,
+    dark,
+    primary,
+    darkText
+  } = infoSectionProps
   return (
-    <InfoContainer id={id} >
+    <InfoContainer id={id} lightBg={lightBg} >
       <InfoWrapper>
-        <InfoRow>
+        <InfoRow imgStart={imgStart} >
           <Column1>
             <TextWrapper>
-              <TopLine>TopLine</TopLine>
-              <Heading>Heading</Heading>
-              <Subtitle>Subtitle</Subtitle>
+              <TopLine>{topLine}</TopLine>
+              <Heading lightText={lightText} >{headLine}</Heading>
+              <Subtitle darkText={darkText} >{description}</Subtitle>
               <BtnWrapper>
-                <Button primary to='home'>Button</Button>
+                <Button
+                  primary={primary}
+                  dark={dark}
+                  to='home'
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  // exact="true"
+                  offset={-80}
+                >
+                  {buttonLabel}
+                </Button>
               </BtnWrapper>
             </TextWrapper>
           </Column1>
           <Column2>
             <ImgWrapper>
-              <Img />
+              <Img src={img.src} alt={alt} />
             </ImgWrapper>
           </Column2>
         </InfoRow>
