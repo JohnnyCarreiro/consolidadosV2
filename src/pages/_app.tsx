5a4f79c3905c63ef9d19f3ec2af79e3944a92c05
Johnny <jcarreiropublicidade@gmail.com>
2021-12-13 03:07:10 -0300

    feat: Add navgation and sidebar to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
 import theme from 'styles/theme'
 import { GlobalStyle } from 'styles/GlobalStyle'
+import Navbar from '@/components/Navbar'
+import { Sidebar } from '@/components/Sidebar'
 
 interface MyAppProps extends AppProps {}
 
 export default function MyApp({Component, pageProps}:MyAppProps){
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
 
     return(
         <ThemeProvider theme={theme} >
-            <Component { ...pageProps }/>
-            <GlobalStyle />
+          <Navbar toggle={toggle} />
+          <Sidebar isOpen={isOpen} toggle={toggle} />
+          <Component { ...pageProps }/>
+          <GlobalStyle />
         </ThemeProvider>
     )
 }
