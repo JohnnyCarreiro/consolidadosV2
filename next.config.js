3cd0d8ae948b0ec2c9997493cee948b134831630
Johnny <jcarreiropublicidade@gmail.com>
2021-12-12 23:11:59 -0300

    fix: Remove image import plugin, new next version has a builtin suport now

--- next.config.js
+++ next.config.js
@@ -2,10 +2,4 @@ const withVideos = require('next-videos')
 const withImages = require('next-images')
 const path = require('path');
 
-module.exports = withVideos(), withImages({
-  esModule: true,
-  inlineImageLimit: false,
-  webpack(config, options) {
-    return config
-  }
-})
+module.exports = withVideos()
